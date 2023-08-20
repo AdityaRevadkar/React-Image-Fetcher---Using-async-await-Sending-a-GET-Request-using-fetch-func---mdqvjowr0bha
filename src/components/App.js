298ba425edb29e8b217149ac41f7ca2045237dd0
AdityaRevadkar <129678008+AdityaRevadkar@users.noreply.github.com>
2023-08-20 17:38:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
+import {useState,useEffect} from 'react';
 const App = () => {
    const [inputNumber, setInputNumber] = useState('');
   const [photoData, setPhotoData] = useState(null);
